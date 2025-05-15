ab6563c7ec6656fdf6e9acaafa90c7d48acef23b
notsiva <sivakarankpr15@gmail.com>
2025-05-15 17:59:59 +0530

    added-projects

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./Project.css";
+import projects from "../../Data/project.json";
 
 const Project = () => {
   return (
-    <div className="projects">
-      <h1 className="title">
-        Currnetly Learining and building projects to showcase in here.{" "}
-        <br /> Stay tuned!
-      </h1>
-    </div>
+    <ul className="projects">
+      {projects.map((projects, id) => {
+        return (
+          <li key={id} className="single-project">
+            <img src={`/assets/${projects.img}`} className="left-side" />
+            <div className="right-side">
+              <h3>{projects.name}</h3>
+              <p>{projects.description}</p>
+              <a href={projects.link} target="_">
+                View Project
+              </a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
